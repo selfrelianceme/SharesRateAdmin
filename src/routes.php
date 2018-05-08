adb519e325a9b33ac48da6677c8551decc572f16
Vladislav <vladwebkit@gmail.com>
2018-05-08 14:56:27 +0300

    ok

--- src/routes.php
+++ src/routes.php
@@ -2,5 +2,6 @@
 
 Route::group(['prefix' => config('adminamazing.path').'/sharesrateadmin', 'middleware' => ['web','CheckAccess']], function() {
 	Route::get('/', 'Selfreliance\SharesRateAdmin\SharesRateAdminController@index')->name('AdminSharesRate');
+	
 	Route::post('store', 'Selfreliance\SharesRateAdmin\SharesRateAdminController@store')->name('AdminSharesRateStore');
 });
